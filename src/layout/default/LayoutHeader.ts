d1d15f7b6064d1e5593ae675c583922dd40a9594
Jason Horvath <kaingnx@gmail.com>
2022-07-14 23:37:15 -0400

    add layout header

---
+++ src/layout/default/LayoutHeader.ts
@@ -0,0 +1,43 @@
+import {html, css, LitElement} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+import '@layout/default/LayoutNavMain';
+
+@customElement('layout-header')
+export class LayoutFooter extends LitElement {
+  static override styles = css`
+    header {
+      display: flex;
+      background-color: #eee;
+      margin: 0;
+      justify-content: space-between;
+      align-items: center;
+      padding: 10px;
+    }
+    
+    .title-container p {
+      display: block;
+      font-size: 14px;
+      padding: 0 1.3em ;
+      margin-top: -10px
+    }
+
+    h1 {
+      margin: 0;
+      padding: .5em;
+    }
+  `;
+
+  @property({type: String}) headerTitle = 'BoiLit';
+
+  override render() {
+    return html`
+      <header class="layout-header">
+        <div class="title-container">
+          <h1 class="title">${this.headerTitle}</h1>
+          <p>BoiLit - The Boiler Plate Kit for Lit</p>
+        </div>
+        <layout-nav-main></layout-nav-main>
+      </header>
+    `;
+  }
+}
