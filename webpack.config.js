62e824677d35058bdd3ba3e19723e117f659ee9e
Jason Horvath <kaingnx@gmail.com>
2022-07-14 23:36:10 -0400

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const Dotenv = require('dotenv-webpack');
+
+require('dotenv').config()
+
+module.exports = {
+  name: "boilit",
+  entry: './src/index.ts',
+  mode: process.env.APP_ENV,
+  devtool: 'source-map',
+  devServer: {
+    liveReload: true,
+    port: process.env.APP_DEV_PORT,
+    open: ['./']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(css|scss|sass)?$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      "@components": path.resolve(__dirname, 'src/components'),
+      "@layout": path.resolve(__dirname, 'src/layout'),
+      "@views": path.resolve(__dirname, 'src/views'),
+    }
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist/js'),
+  },
+  plugins: [
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+        template: 'dist/index.html'
+    }),
+    ...((/true/i).test(process.env.APP_ANALYZE) ? [new BundleAnalyzerPlugin()] : [])
+  ],
+};
