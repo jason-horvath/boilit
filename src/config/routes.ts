73e363e5ca606b44589712c8da5b8d94e86daffb
Jason Horvath <kaingnx@gmail.com>
2022-07-16 12:18:00 -0400

    update routes

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,9 +1,9 @@
 import RouteCollection from '../routing/RouteCollection';
-import Route from '../routing/Route';
+import Route from '@routing/Route';
 
 const routes = new RouteCollection();
 
-routes.add('home', new Route('home', 'home-view', require('@views/HomeView'), false));
-routes.add('about', new Route('about', 'about-view', require('@views/AboutView'), false));
+routes.add('/', new Route('home', 'home-view', require('@views/HomeView'), false));
+routes.add('/about', new Route('about', 'about-view', require('@views/AboutView'), false));
 
 export default routes;
