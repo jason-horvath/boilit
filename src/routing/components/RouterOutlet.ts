c6ec0ebac37612605bedfb3dd6bbc3560f9c820b
Jason Horvath <kaingnx@gmail.com>
2022-07-15 23:09:38 -0400

    route outlet

---
+++ src/routing/components/RouterOutlet.ts
@@ -0,0 +1,30 @@
+import {html, css, LitElement} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+import RouteCollection from '../RouteCollection';
+
+@customElement('router-outlet')
+export default class RouterOutlet extends LitElement {
+  static override styles = css`p { color: blue }`;
+
+  @property() routeCollection: RouteCollection | null = null;
+
+  @property({type: String}) activeTag = 'Tst';
+
+  constructor(routeCollection: RouteCollection) {
+    super()
+    this.routeCollection = routeCollection;
+  } 
+
+  override connectedCallback() {
+    super.connectedCallback();
+    console.log('sadfasdfa')
+    window.addEventListener('route-navigate', (e: any) => {
+      // console.log(e);
+      this.activeTag = e.detail.uri;
+    })
+  }
+
+  override render() {
+    return html`<span>${this.activeTag}</span>`;
+  }
+}
