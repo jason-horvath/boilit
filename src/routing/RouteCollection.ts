2e0de96745565bf5f6ea186d4d26a833658e09ee
Jason Horvath <kaingnx@gmail.com>
2022-07-17 19:48:34 -0400

    moved

--- src/routing/RouteCollection.ts
+++
@@ -1,9 +0,0 @@
-import Route from '@routing/Route';
-
-export default class RouteCollection {
-    routeMap = new Map<String, Route>()
-
-    add(key: string, route: Route) {
-        this.routeMap.set(key, route);
-    }
-}
