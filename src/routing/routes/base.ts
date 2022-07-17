76e27806fcb7968aae9a11788a8cbd0dfb86e1fb
Jason Horvath <kaingnx@gmail.com>
2022-07-17 19:51:04 -0400

    base routes

---
+++ src/routing/routes/base.ts
@@ -0,0 +1,9 @@
+import RouteCollection from '../core/RouteCollection';
+import Route from '../core/Route';
+
+const baseRoutes = new RouteCollection();
+
+baseRoutes.add('/', new Route('/', 'default-index-view', require('../views/DefaultIndexView')));
+baseRoutes.add('/404', new Route('/404', 'default-not-found-view', require('../views/DefaultNotFoundView')));
+
+export default baseRoutes;
