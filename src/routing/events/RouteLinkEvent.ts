9109045921668feec59fb3cfc6d8e44f1ddd0a14
Jason Horvath <kaingnx@gmail.com>
2022-07-17 19:50:58 -0400

    interface for custom event options

--- src/routing/events/RouteLinkEvent.ts
+++ src/routing/events/RouteLinkEvent.ts
@@ -1,7 +1,3 @@
-export default class RouteLinkEvent extends Event {
-  uri: string = '';
-  constructor(uri: string) {
-    super('route-navigate');
-    this.uri = uri;
-  }
+export default interface RouteLinkEvent {
+  uri: string;
 }
