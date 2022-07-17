02086fe4ef31490dcbfafe541f83f8217d97dcf5
Jason Horvath <kaingnx@gmail.com>
2022-07-17 19:46:34 -0400

    use base routes

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,9 +1,7 @@
-import RouteCollection from '../routing/RouteCollection';
-import Route from '@routing/Route';
+import routes from '../routing/routes/base';
+import Route from '@routing/core/Route';
 
-const routes = new RouteCollection();
-
-routes.add('/', new Route('home', 'home-view', require('@views/HomeView'), false));
-routes.add('/about', new Route('about', 'about-view', require('@views/AboutView'), false));
+routes.add('/', new Route('home', 'home-view', require('@views/HomeView')));
+routes.add('/about', new Route('about', 'about-view', require('@views/AboutView')));
 
 export default routes;
