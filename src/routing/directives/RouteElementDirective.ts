66a040ff253270440401358209c9520842f466d2
Jason Horvath <kaingnx@gmail.com>
2022-07-18 08:03:36 -0400

    give params to template

--- src/routing/directives/RouteElementDirective.ts
+++ src/routing/directives/RouteElementDirective.ts
@@ -2,9 +2,8 @@ import {html, unsafeStatic} from 'lit/static-html.js';
 import { Directive, directive } from 'lit/directive.js';
 
 class RouteElementDirective extends Directive {
- 
-  render(tag: string) {
-    return html`<${unsafeStatic(tag)} .params=${{test: 10}}></${unsafeStatic(tag)}`;
+  render(tag: string, params: Map<String, String>) {
+    return html`<${unsafeStatic(tag)} .params=${params}></${unsafeStatic(tag)}>`;
   }
 }
 
