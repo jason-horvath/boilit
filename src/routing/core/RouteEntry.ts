025a841c168a512f812e69a4620b47d0b462bdd5
Jason Horvath <kaingnx@gmail.com>
2022-07-18 08:03:15 -0400

    update routeentry methods

--- src/routing/core/RouteEntry.ts
+++ src/routing/core/RouteEntry.ts
@@ -2,18 +2,34 @@ import Route from './Route';
 
 export default class RouteEntry {
   private path: String;
-  private route: Route;
+  private route: Route | undefined;
 
-  constructor(path: String, route: Route) {
+  constructor(path: String, route: Route | undefined) {
     this.path = path;
     this.route = route;
   }
 
-  getPath() {
+  getPath(): String {
     return this.path;
   }
 
-  getRoute() {
+  getRoute(): Route | undefined  {
     return this.route;
+  }
+
+  getParams(): Map<String , number> {
+    let params: Map<String, number> = new Map();
+    
+    this.path.split('/').map((param, key) => {
+      if (param.startsWith(':')) {
+        params.set(param, key);
+      }
+    })
+
+    return params;
+  }
+
+  hasParams(): boolean {
+    return [...this.getParams().keys()].length > 0;
   }
 }
