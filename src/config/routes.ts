c87b0628c80da86dfe671073472d3bb57f5d2cb2
Jason Horvath <kaingnx@gmail.com>
2022-07-18 06:06:06 -0400

    update path

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,7 +1,7 @@
 import routes from '../routing/routes/base';
-import Route from '@routing/core/Route';
+import Route from '../routing/core/Route';
 
-routes.add('/', new Route('home', 'home-view', require('@views/HomeView')));
-routes.add('/about', new Route('about', 'about-view', require('@views/AboutView')));
+routes.add('/', new Route('home-view', require('@views/HomeView')));
+routes.add('/about', new Route('about-view', require('@views/AboutView')));
 
 export default routes;
