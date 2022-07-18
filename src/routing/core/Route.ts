a71952c0c3d03ab2a48e0d742697387f88ed5fd5
Jason Horvath <kaingnx@gmail.com>
2022-07-18 06:06:34 -0400

    uri is redundant, stored as entry key

--- src/routing/core/Route.ts
+++ src/routing/core/Route.ts
@@ -2,20 +2,17 @@ import { LitElement } from 'lit';
 import RouteMeta from '../types/RouteMeta';
 
 export default class Route {
-  uri: string = '';
   customElementName: string = '';
   component: LitElement = {} as LitElement;
   meta: RouteMeta = {};
   isProtected: boolean = false;
 
   constructor(
-    uri: string,
     customElementName: string,
     component: LitElement,
     isProtected: boolean = false,
     meta: Object = {}
   ) {
-    this.uri = uri;
     this.customElementName = customElementName;
     this.component = component;
     this.isProtected = isProtected;
