a66d13870aacdcbebb6fe828250ecaafb5ff52ef
Jason Horvath <kaingnx@gmail.com>
2022-07-19 10:34:11 -0400

    update text

--- src/views/HomeView.ts
+++ src/views/HomeView.ts
@@ -17,7 +17,7 @@ export class HomeView extends LitElement {
       <div class="home-view">
         <h1>${this.homeTitle}</h1>
         <p>This is just and example of where a view slot would go.</p>
-        <p>The links up to do not go anywhere, as they would require routing or some static content at those paths. It is just to illustrate the example of rendering links.</p>
+        <p>The navigation links are using the official <a href="https://www.npmjs.com/package/@larzilla/boilit-router" target="_blank">BoiLit Router</a> package for navigation.</p>
         <p>And now.... the moment you have all been waiting for. The obligatory counter.</p>
         <app-counter></app-counter>
       </div>
